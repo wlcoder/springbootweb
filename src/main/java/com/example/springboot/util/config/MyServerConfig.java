d209a183aed27ddf9e2e59d3a4638784118e98a9
wlong <wanglong@ysstech.com>
2019-11-28 09:40:19 +0800

    /

--- src/main/java/com/example/springboot/util/config/MyServerConfig.java
+++ src/main/java/com/example/springboot/util/config/MyServerConfig.java
@@ -18,22 +18,22 @@ public class MyServerConfig {
 
     //注册三大组件
     @Bean
-    public ServletRegistrationBean myServlet(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(),"/myServlet");
+    public ServletRegistrationBean myServlet() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new MyServlet(), "/myServlet");
         registrationBean.setLoadOnStartup(1);
         return registrationBean;
     }
 
     @Bean
-    public FilterRegistrationBean myFilter(){
+    public FilterRegistrationBean myFilter() {
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();
         registrationBean.setFilter(new MyFilter());
-        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        registrationBean.setUrlPatterns(Arrays.asList("/hello", "/myServlet"));
         return registrationBean;
     }
 
     @Bean
-    public ServletListenerRegistrationBean myListener(){
+    public ServletListenerRegistrationBean myListener() {
         ServletListenerRegistrationBean<MyListener> registrationBean = new ServletListenerRegistrationBean<>(new MyListener());
         return registrationBean;
     }
@@ -41,13 +41,13 @@ public class MyServerConfig {
 
     //配置嵌入式的Servlet容器
     @Bean
-    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer(){
+    public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
         return new EmbeddedServletContainerCustomizer() {
 
             //定制嵌入式的Servlet容器相关的规则
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
-                container.setPort(8083);
+                container.setPort(8888);
             }
         };
     }

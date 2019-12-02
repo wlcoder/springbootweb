711d4f425eadd661556a8e733125b0b9db1be894
wlong <wanglong@ysstech.com>
2019-12-02 15:38:20 +0800

    添加swagger测试

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -6,6 +6,9 @@ import com.example.springboot.service.DepartmentService;
 import com.example.springboot.service.EmployeeService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 import java.util.List;
 
+@Api(tags = "EmployeeController", description = "员工controller测试swagger")
 @Controller
 public class EmployeeController {
     @Autowired
@@ -21,6 +25,7 @@ public class EmployeeController {
     @Autowired
     private DepartmentService departmentService;
 
+    @ApiOperation(value = "查询所有员工", notes = "获取用户列表")
     //查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model, @RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) {
@@ -32,6 +37,7 @@ public class EmployeeController {
     }
 
     //员工添加页面
+    @ApiOperation(value = "跳转到员工页面", notes = "跳转到员工页面")
     @GetMapping("/emp")
     public String toAddPage(Model model) {
         //来到添加页面,查出所有的部门，在页面显示
@@ -41,6 +47,8 @@ public class EmployeeController {
     }
 
     //员工添加
+    @ApiOperation(value = "员工添加", notes = "员工添加")
+    @ApiImplicitParam(name = "employee", value = "员工信息employee", required = true, dataType = "Employee")
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
         //保存员工
@@ -49,6 +57,8 @@ public class EmployeeController {
     }
 
     //修改页面，查出当前员工，在页面回显
+    @ApiOperation(value = "跳转到员工修改页面", notes = "跳转到员工修改页面")
+    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path",dataType = "Integer")
     @GetMapping("/emp/{id}")
     public String toEditPage(@PathVariable("id") Integer id, Model model) {
         Employee employee = employeeService.getEmployee(id);
@@ -60,18 +70,21 @@ public class EmployeeController {
     }
 
     //员工修改
+    @ApiOperation(value = "员工修改", notes = "员工修改")
+    @ApiImplicitParam(name = "employee", value = "员工信息employee", required = true, dataType = "Employee")
     @PutMapping("/emp")
     public String updateEmployee(Employee employee) {
         employeeService.updateEmployee(employee);
         return "redirect:/emps";
     }
 
-    //员工删除
-    @DeleteMapping("/emp/{id}")
+    //员工删除  paramType="path"解决传入类型不匹配
+    @ApiOperation(value = "员工删除", notes = "员工删除")
+    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path",dataType = "Integer")
+    @RequestMapping ("/emps/{id}")
     public String deleteEmployee(@PathVariable("id") Integer id) {
         employeeService.delEmployee(id);
         return "redirect:/emps";
     }
-
 
 }

d83867b1e8317af6fac10b915d9ca070e1d71bc4
wlong <wanglong@ysstech.com>
2019-12-08 11:21:27 +0800

    /

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.springboot.entity.Department;
 import com.example.springboot.entity.Employee;
 import com.example.springboot.service.DepartmentService;
 import com.example.springboot.service.EmployeeService;
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
@@ -28,10 +29,12 @@ public class EmployeeController {
     @ApiOperation(value = "查询所有员工", notes = "获取用户列表")
     //查询所有员工返回列表页面
     @GetMapping("/emps")
-    public String list(Model model, @RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) {
-        PageHelper.startPage(start, size, "id desc");
+    public String list(Model model, @RequestParam(defaultValue = "1", value = "pageNum") Integer pageNum) {
+        Page p = PageHelper.startPage(pageNum, 5);
         List<Employee> employees = employeeService.getAllEmployee();
-        PageInfo<Employee> page = new PageInfo<>(employees);
+        //pageInfo 只显示分页pagesize 的数据
+        //PageInfo<Employee> pageInfo = new PageInfo<>(employees);
+        PageInfo page = new PageInfo<>(p.getResult());
         model.addAttribute("page", page);
         return "emp/list";
     }
@@ -58,7 +61,7 @@ public class EmployeeController {
 
     //修改页面，查出当前员工，在页面回显
     @ApiOperation(value = "跳转到员工修改页面", notes = "跳转到员工修改页面")
-    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path",dataType = "Integer")
+    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path", dataType = "Integer")
     @GetMapping("/emp/{id}")
     public String toEditPage(@PathVariable("id") Integer id, Model model) {
         Employee employee = employeeService.getEmployee(id);
@@ -80,8 +83,8 @@ public class EmployeeController {
 
     //员工删除  paramType="path"解决传入类型不匹配
     @ApiOperation(value = "员工删除", notes = "员工删除")
-    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path",dataType = "Integer")
-    @RequestMapping ("/emps/{id}")
+    @ApiImplicitParam(name = "id", value = "员工id", required = true, paramType = "path", dataType = "Integer")
+    @RequestMapping("/emps/{id}")
     public String deleteEmployee(@PathVariable("id") Integer id) {
         employeeService.delEmployee(id);
         return "redirect:/emps";

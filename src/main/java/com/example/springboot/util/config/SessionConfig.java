edf26e9bbff7849e25894f72e259ae3f8804e695
wlong <wanglong@ysstech.com>
2019-12-20 10:47:21 +0800

    session

--- src/main/java/com/example/springboot/util/config/SessionConfig.java
+++ src/main/java/com/example/springboot/util/config/SessionConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60*30)
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 30)
 public class SessionConfig {
 }
